d7581f979860e366666acb5010eb7208c99c069d
Alexander <alexael@ntnu.no>
2020-12-17 10:30:16 +0100

    Exam commit

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -8,6 +8,49 @@ class EditUser extends LitElement {
   }
 
   // din kode her
+  async getInfo() {
+    await fetch(`api/fetchUser.php`, {
+            method: 'GET',
+            credentials: 'include'
+            }).then ( res => res.json()).then (data => {
+                console.log("Authenticated user: " + JSON.stringify(data) );
+                this.user.id = data.uid;
+                this.user.email = data.email;
+                this.user.name = data.name;
+            })
+  }
+
+  editUser() {
+    var nameData = this.shadowRoot.getElementById('name').value;
+    var emailData = this.shadowRoot.getElementById('email').value;
+    var passwordData1 = this.shadowRoot.getElementById('password1').value;
+    var passwordData2 = this.shadowRoot.getElementById('password2').value;
+    var data = {
+      uid : this.id,
+      email: emailData,
+      name: nameData
+    }
+    if(passwordData1 === passwordData2) {
+      this.shadowRoot.getElementById('passErr').style.display='none';
+      data.password = passwordData1;
+    } 
+    else {
+      this.shadowRoot.getElementById('passErr').style.display='block';
+      return
+    }
+    
+    await fetch(`api/updateUser.php`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',  
+      },
+      body: JSON.stringify(data),
+
+        }).then(res => res.json())
+        .then(data => {console.log("Success", data);
+      }).catch((error) => {console.error("Error in post")})
+    console.log(data.email)
+  }
 
 }
 customElements.define('edit-user', EditUser);
